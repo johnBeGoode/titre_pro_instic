85cf4c63cdf4e5ba59693a2225f8b5d01ad940f7
Thierry <thierry.odier26@gmail.com>
2020-02-21 16:10:44 +0100

    header et footer ajoutés

--- views/include-parts/footer.php
+++ views/include-parts/footer.php
@@ -1,10 +1,44 @@
-    <footer>footerrrrr</footer>
+<footer>
+    <div id="links">
+        <div class="div_footer">
+            <h5>Azerty</h5>
+            <ul>
+                <li><a href="#">Lorem</a></li> 
+                <li><a href="#">Consectur</a></li>
+                <li><a href="#">Nihili</a></li>
+            </ul>
+        </div>
 
-    <script
-    src="https://code.jquery.com/jquery-3.4.1.min.js"
-    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-    crossorigin="anonymous"></script> 
-    <script src="/public/js/app.js"></script> 
+        <div class="div_footer">
+            <h5>Bzerty</h5>
+            <ul>
+                <li><a href="#">Extant</a></li>
+                <li><a href="#">Fundita</a></li>
+                <li><a href="#">Asbolus</a></li>
+            </ul>
+        </div>
+
+        <div class="div_footer">
+            <h5>Czerty</h5>
+            <ul>
+                <li><a href="#">Chill Out</a></li>
+                <li><a href="#">Raptio</a></li>
+                <li><a href="#">Palaestri</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div id="reseau_social"> 
+        <a href="#"><img id ="logofb" class="logo_rs" src="public/images/ico_fb.png" alt="facebook"></a>
+        <a href="#"><img class="logo_rs" src="public/images/ico_in.png" alt="linkedin"></a>
+        <a href="#"><img class="logo_rs" src="public/images/ico_insta.png" alt="instagram"></a>
+        <a href="#"><img class="logo_rs" src="public/images/ico_twitter.png" alt="twitter"></a>
+    </div>
+
+    <div id="menu_footer">
+        <a href="#">Présentation</a>  | <a href="#">A propos</a> | <a href="#">Contact</a>  | <a href="#">Mentions légales</a>
+    </div>
+</footer>
 
 </body>
 </html>
--- views/include-parts/header.php
+++ views/include-parts/header.php
@@ -2,10 +2,34 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>  
-    <link href="/public/styles.css" rel="stylesheet" type="text/css"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="/public/styles.css">
     <link rel="icon" href="favicon.ico">
     <meta name="description" content="Une description de la page..."/>
-    <meta name="author" content="Odier Thierry"/>
+    <meta name="author" content="Jonathan Martin"/>
 </head>
 <body>
+    <header>
+        <div id="logo_header">
+            <img src="https://cdn.svgporn.com/logos/chromatic-icon.svg" alt="logo blog" id="logo_image">
+            <p>Chromatic SinemA</p>
+        </div>
+        <div id="menu">
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Cinéma</a></li>
+                    <li><a href="">Série</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div id="button">
+            <button type="button" class="btn btn-primary">Créer un compte</button>
+            <button type="button" class="btn btn-primary">Se connecter</button>
+        </div>
+    </header>
+    
+   
