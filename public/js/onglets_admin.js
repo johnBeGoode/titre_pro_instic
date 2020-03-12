d532a5aa86e1e0d422fd72a7816b4512d267abb8
john119 <jonathan.martin084@gmail.com>
2020-03-12 18:46:01 +0100

    onglet admin ok (amélioration au niveau affichage)

--- public/js/onglets_admin.js
+++ public/js/onglets_admin.js
@@ -1,27 +1,28 @@
-var afficherOnglet = function(a) {
-    var li = a.parentNode
-    var div = a.parentNode.parentNode.parentNode
+let afficherOnglet = function(a) {
+    let li = a.parentNode
+    let div = a.parentNode.parentNode.parentNode.parentNode
+    // let div = document.getElementById('wrapper')
 
-    if (li.classList.contains('active')) {
-        return false
-    }
-
+    // if (li.classList.contains('active')) {
+    //     return false
+    // }
+    
     // on retire la class active à l'onglet actif
     div.querySelector('.tabs .active').classList.remove('active')
     // j'ajoute la class active à l'onglet actuel
     li.classList.add('active')
-
     // on retire la class active du contenu actif
     div.querySelector('.tab-content.active').classList.remove('active')
-    // on ajoute la classe active au contenu actif
+    // on ajoute la classe active au nouveau contenu actif
     div.querySelector(a.getAttribute('href')).classList.add('active')
+    
 }
 
 // ajout évènement clic sur les liens
-var tabs = document.querySelectorAll('.tabs a')
+let tabs = document.querySelectorAll('.tabs a')
 // on applique une fonction sur chacun de ces liens avec une boucle
-for (var i = 0; i < tabs.length; i++) {
-    var link = tabs[i]
+for (let i = 0; i < tabs.length; i++) {
+    let link = tabs[i]
     link.addEventListener('click', function (e) {
         afficherOnglet(this);
     })
--- views/admin.php
+++ views/admin.php
@@ -2,113 +2,120 @@
 
 <div id="page-admin">
     <h1>Page d'administration</h1>
+    <div id="wrapper">
+        <nav>
+            <ul class="tabs">
+                <li><a href="administration?page=mon_compte">Mon compte</a></li>
+                <li class="active"><a href="#articles">Articles</a></li>
+                <li><a href="#categories">Categories</a></li>
+                <li><a href="#commentaires">Commentaires</a></li>
+                <li><a href="#users">Utilisateurs</a></li>
+                <li><a href="/deconnexion">Deconnexion</a></li>
+            </ul>
+        </nav>
 
-    <nav>
-        <ul class="tabs">
-            <li><a href="administration?page=mon_compte">Mon compte</a></li>
-            <li class="active"><a href="#articles">Articles</a></li>
-            <li><a href="#categories">Categories</a></li>
-            <li><a href="#commentaires">Commentaires</a></li>
-            <li><a href="#users">Utilisateurs</a></li>
-            <li><a href="/deconnexion">Deconnexion</a></li>
-        </ul>
-    </nav>
-
-    <!-- <p>Il y a actuellement <?//= $nbMovies ?> :</p> -->
-    <div class="tabs-content">
-        <table id="articles" class="admin-articles table table-striped tab-content">
-            <thead>
-                <tr>
-                    <th>Image</th>
-                    <th>Titre</th>
-                    <th>Date d'ajout</th>
-                    <th>Synopsis</th>
-                    <th>Nb de commentaires</th>
-                    <th>Publié</th>
-                </tr>
-            <thead>
-
-            <tbody>
-                <?php foreach ($movies as $movie): ?>
+        <!-- <p>Il y a actuellement <?//= $nbMovies ?> :</p> -->
+        <div class="tabs-content">
+            <table id="articles" class="admin-articles table table-striped tab-content active">
+                <thead>
                     <tr>
-                        <td><img src="<?= $movie->getPicture() ?>" alt=""></td>
-                        <td><?= $movie->getTitle() ?></td>
-                        <td><?= $movie->getDateAdd() ?></td>
-                        <td><?= substr($movie->getResume(),0,200) ?> ...</td>
-                        <!-- <td><a href="?modifier='<?//= $movie->getId() ?>'">Modifier</a> | <a href="?supprimer='<?//= $movie->getId() ?> '">Supprimer</a></td><br> -->
+                        <th>Image</th>
+                        <th>Titre</th>
+                        <th>Date d'ajout</th>
+                        <th>Synopsis</th>
+                        <th>Nb de commentaires</th>
+                        <th>Publié</th>
                     </tr>
-                <?php endforeach ?>
-            </tbody>    
-        </table>
-        <br>
-        <br>
-        <table id="commentaires" class="admin-comments table table-striped tab-content active">
-            <thead>
-                <tr>
-                    <th>Commentaires</th>
-                    <th>Date d'ajout</th>
-                    <th>Article</th>
-                    <th>User</th>
-                </tr>
-            <thead>
+                <thead>
 
-            <tbody>
-                <?php foreach ($comments as $comment): ?>
-                    <tr>
-                        <td><?= $comment->getComment() ?></td>
-                        <td><?= $comment->getDateAdd() ?></td>
-                        <td><?= $comment->getMovieId() ?></td>
-                        <td><?= $comment->getUser()->getName() ?></td>
-                    </tr>
-                <?php endforeach ?>
-            </tbody>    
-        </table>
-        <br>
-        <br>
-        <table id="users" class="admin-users table table-striped tab-content">
-            <thead>
-                <tr>
-                    <th>Avatar</th>
-                    <th>Nom</th>
-                    <th>Password</th>
-                    <th>Email</th>
-                    <th>Rôle</th>
-                    <th>Date d'enregistrement</th>
-                </tr>
-            <thead>
+                <tbody>
+                    <?php foreach ($movies as $movie): ?>
+                        <tr>
+                            <td><img src="<?= $movie->getPicture() ?>" alt=""></td>
+                            <td><?= $movie->getTitle() ?></td>
+                            <td><?= $movie->getDateAdd() ?></td>
+                            <td><?= substr($movie->getResume(),0,200) ?> ...</td>
+                            <!-- <td><a href="?modifier='<?//= $movie->getId() ?>'">Modifier</a> | <a href="?supprimer='<?//= $movie->getId() ?> '">Supprimer</a></td><br> -->
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>    
+            </table>
 
-            <tbody>
-                <?php foreach ($users as $user): ?>
-                    <tr>
-                        <td><img src="<?= $user->getAvatar() ?>" alt=""></td>
-                        <td><?= $user->getName() ?></td>
-                        <td><?= $user->getPassword() ?></td>
-                        <td><?= $user->getEmail() ?></td>
-                        <td><?= $user->getRole() ?></td>
-                        <td><?= $user->getDateRegistration() ?></td>
-                    </tr>
-                <?php endforeach ?>
-            </tbody>    
-        </table>
-        <br>
-        <br>
-        <table id="categories" class="admin-categories table table-striped tab-content">
-            <thead>
-                <tr>
-                    <th>Nom</th>
-                    <th>Nb de films</th>
-                </tr>
-            <thead>
+            <br>
+            <br>
 
-            <tbody>
-                <?php foreach ($categories as $category): ?>
+            <table id="commentaires" class="admin-comments table table-striped tab-content">
+                <thead>
                     <tr>
-                        <td><?= $category->getName() ?></td>
-                        <td><?= $category->getMoviesByCategory() ?></td>
+                        <th>Commentaires</th>
+                        <th>Date d'ajout</th>
+                        <th>Article</th>
+                        <th>User</th>
                     </tr>
-                <?php endforeach ?>
-            </tbody>    
-        </table>
+                <thead>
+
+                <tbody>
+                    <?php foreach ($comments as $comment): ?>
+                        <tr>
+                            <td><?= $comment->getComment() ?></td>
+                            <td><?= $comment->getDateAdd() ?></td>
+                            <td><?= $comment->getMovieId() ?></td>
+                            <td><?= $comment->getUser()->getName() ?></td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>    
+            </table>
+
+            <br>
+            <br>
+
+            <table id="users" class="admin-users table table-striped tab-content">
+                <thead>
+                    <tr>
+                        <th>Avatar</th>
+                        <th>Nom</th>
+                        <th>Password</th>
+                        <th>Email</th>
+                        <th>Rôle</th>
+                        <th>Date d'enregistrement</th>
+                    </tr>
+                <thead>
+
+                <tbody>
+                    <?php foreach ($users as $user): ?>
+                        <tr>
+                            <td><img src="<?= $user->getAvatar() ?>" alt=""></td>
+                            <td><?= $user->getName() ?></td>
+                            <td><?= $user->getPassword() ?></td>
+                            <td><?= $user->getEmail() ?></td>
+                            <td><?= $user->getRole() ?></td>
+                            <td><?= $user->getDateRegistration() ?></td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>    
+            </table>
+
+            <br>
+            <br>
+
+            <table id="categories" class="admin-categories table table-striped tab-content">
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Nb de films</th>
+                    </tr>
+                <thead>
+
+                <tbody>
+                    <?php foreach ($categories as $category): ?>
+                        <tr>
+                            <td><?= $category->getName() ?></td>
+                            <td><?= $category->getMoviesByCategory() ?></td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>    
+            </table>
+        </div>
     </div>
 </div>
 
