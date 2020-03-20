5d333e54ca6c635fcfb54adcb126f3e460044eab
Thierry <thierry.odier26@gmail.com>
2020-03-20 17:36:45 +0100

    gestion de l'effacement de la variable de session contenant les notifs

--- public/js/notifs.js
+++ public/js/notifs.js
@@ -1,10 +1,16 @@
 $(function(){
-    /*
-   $('.notif').delay(3000).slideUp(function(){
+
+    $('.notif').slideDown(500,function(){
+        $('.notif').first().delay(2000).fadeOut(2000, function suivante() {
+            $(this).next('.notif').delay(2000).fadeOut(2000,suivante);
+        })
+        ajaxDeleteNotifsSession()
+    }).css("display","flex");
+
+    function ajaxDeleteNotifsSession(){
         $.ajax({
-            url: 'public/ajax/unset.php', // ../../controllers/function
+            url: 'public/ajax/unset.php',
             type: 'GET',
-            // dataType: 'script',
             success: function(reponse, statut) {
                 console.log(reponse)
             },
@@ -12,16 +18,7 @@ $(function(){
                 alert('appel ajax échoué')
             }
         })
-    })
-    */
-    
+    }
 
-   $('.notif').each(function(){
-        $(this).slideDown(500,function(){
-            $('.notif').first().delay(2000).fadeOut(2000, function suivante() {
-                $(this).next('.notif').delay(2000).fadeOut(2000,suivante);
-            })
-        }).css("display","flex");
-    });
     
 })
