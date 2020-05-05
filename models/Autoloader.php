f24ef65f681206a89ebfbf9a25d90ae243b1a535
johnBeGoode <you@example.com>
2020-05-05 18:05:49 +0200

    Mise en forme fichier Autoloader

--- models/Autoloader.php
+++ models/Autoloader.php
@@ -1,12 +1,12 @@
 <?php 
 namespace App;
 
-class Autoloader{
+class Autoloader {
 
-    static function autoload() {
-        spl_autoload_register(function ($class) {            
-            $class = str_replace('App\\','',$class);
-            $class = str_replace('\\','/',$class);            
+    public static function autoload() {
+        spl_autoload_register(function($class) {    
+            $class = str_replace('App\\','', $class);
+            $class = str_replace('\\','/', $class);
             require  '../models/' . $class . '.php';
         });
     }
